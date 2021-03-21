b7677f834d2d735c3a24e6db4778f241860c44f8
ding-ma <ding.ma@mail.mcgill.ca>
2021-03-21 19:28:01 +0000

    two layer tests are done

--- mini-project-3/run.py
+++ mini-project-3/run.py
@@ -20,74 +20,165 @@ from itertools import islice
 import numpy as np
 
 
-data_preprocess_config = {
-    "threshold": False,
-    "normalize": True,
-    "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
-}
+# data_preprocess_config = {
+#     "threshold": False,
+#     "normalize": True,
+#     "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+# }
 
-gradient_config = {"batch_size": 20, "learn_rate_init": 0.0005, "reg_lambda": 0.20, "num_epochs": 5, "L2": False}
+# gradient_config = {"batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15, "L2": False}
+
+# model_config_0_layer = {
+#     "input_dim": 28*28,
+#     "output_dim": 10,
+#     "output_fnc": Softmax(),
+# }
+# mlp = NoLayer(model_config_0_layer, **gradient_config)
+
+# model_config_1_layer = {
+#     "input_dim": 28 * 28,
+#     "hidden_dim": 128,
+#     "output_dim": 10,
+#     "hiddent_fnc": ReLU(),
+#     "output_fnc": Softmax(),
+# }
+# mlp = OneLayer(model_config_1_layer, **gradient_config)
 
 if __name__ == "__main__":
     np.random.seed(0)
+    
+    experiments = [
+        (
+            {
+                "input_dim": 28 * 28,
+                "hidden_1_dim": 128,
+                "hidden_2_dim": 128,
+                "output_dim": 10,
+                "hiddent_1_fnc": ReLU(),
+                "hiddent_2_fnc": ReLU(),
+                "output_fnc": Softmax()
+            }, 
+            {
+                "batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15,  "L2": False, "anneal":True
+            },
+            {
+                "threshold": False,
+                "normalize": True,
+                "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+            }
+        ),
+        (
+            {
+                "input_dim": 28 * 28,
+                "hidden_1_dim": 128,
+                "hidden_2_dim": 128,
+                "output_dim": 10,
+                "hiddent_1_fnc": Sigmoid(),
+                "hiddent_2_fnc": Sigmoid(),
+                "output_fnc": Softmax()
+            }, 
+            {
+                "batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15,  "L2": False, "anneal":True
+            },
+            {
+                "threshold": False,
+                "normalize": True,
+                "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+            }
+        ),
+        (
+            {
+                "input_dim": 28 * 28,
+                "hidden_1_dim": 128,
+                "hidden_2_dim": 128,
+                "output_dim": 10,
+                "hiddent_1_fnc": TanH(),
+                "hiddent_2_fnc": TanH(),
+                "output_fnc": Softmax()
+            }, 
+            {
+                "batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15,  "L2": False, "anneal":True
+            },
+            {
+                "threshold": False,
+                "normalize": True,
+                "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+            }
+        ),
+        (
+            {
+                "input_dim": 28 * 28,
+                "hidden_1_dim": 128,
+                "hidden_2_dim": 128,
+                "output_dim": 10,
+                "hiddent_1_fnc": ReLU(),
+                "hiddent_2_fnc": ReLU(),
+                "output_fnc": Softmax()
+            }, 
+            {
+                "batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15,  "L2": True, "anneal":True
+            },
+            {
+                "threshold": False,
+                "normalize": True,
+                "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+            }
+        ),
+        (
+            {
+                "input_dim": 28 * 28,
+                "hidden_1_dim": 128,
+                "hidden_2_dim": 128,
+                "output_dim": 10,
+                "hiddent_1_fnc": ReLU(),
+                "hiddent_2_fnc": ReLU(),
+                "output_fnc": Softmax()
+            }, 
+            {
+                "batch_size": 10, "learn_rate_init": 0.0002, "reg_lambda": 0.1, "num_epochs": 15,  "L2": False, "anneal":True
+            },
+            {
+                "threshold": False,
+                "normalize": False,
+                "augment_data": {"rotate": False, "shift": False, "zoom": False, "shear": False, "all": False}
+            }
+        ),
+    ]
 
-    experiment_description = f"""
-    Augmented all the data
-    No L2
+    for model_config, gradient_config, preprocess_param in experiments:
+        experiment_description = f"""
+        Gradient Parameters
+        {gradient_config}
 
-    Experiment Parameters
-    {gradient_config}
+        Preprocess Parameters
+        {preprocess_param}
 
-    Preprocess Parameters
-    {data_preprocess_config}
+        Model Parameters
+        {model_config}
+        """
 
-    """
+        mlp = TwoLayer(model_config, **gradient_config)
 
-    model_config_0_layer = {
-        "input_dim": 28*28,
-        "output_dim": 10,
-        "output_fnc": Softmax(),
-    }
-    # mlp = NoLayer(model_config_0_layer, **gradient_config)
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            level=logging.INFO,
+            datefmt="%Y-%m-%d %H:%M:%S",
+            handlers=[
+                logging.FileHandler(filename=f"logs/{mlp.file_name}.log"),
+                logging.StreamHandler(sys.stdout),
+            ],
+        )
 
-    model_config_1_layer = {
-        "input_dim": 28 * 28,
-        "hidden_dim": 128,
-        "output_dim": 10,
-        "hiddent_fnc": ReLU(),
-        "output_fnc": Softmax(),
-    }
-    # mlp = OneLayer(model_config_1_layer, **gradient_config)
-
-    model_config_2_layer = {
-        "input_dim": 28 * 28,
-        "hidden_1_dim": 128,
-        "hidden_2_dim": 128,
-        "output_dim": 10,
-        "hiddent_1_fnc": ReLU(),
-        "hiddent_2_fnc": ReLU(),
-        "output_fnc": Softmax(),
-    }
-    mlp = TwoLayer(model_config_2_layer, **gradient_config)
-
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s",
-        level=logging.INFO,
-        datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[
-            # logging.FileHandler(filename=f"logs/{mlp.file_name}.log"),
-            logging.StreamHandler(sys.stdout),
-        ],
-    )
-
-    logging.info(experiment_description)
+        logging.info(experiment_description)
 
 
-    (train_array, y_train), test_array, y_test = aquire_data(**data_preprocess_config)
+        (train_array, y_train), test_array, y_test = aquire_data(**preprocess_param)
 
-    mlp.fit(train_array, y_train, test_array, y_test)
-    y_pred = mlp.predict(test_array)
+        mlp.fit(train_array, y_train, test_array, y_test)
+        y_pred = mlp.predict(test_array)
 
-    logging.info(f"Final test accuracy {mlp.compute_acc(y_pred, y_test)}")
-    mlp.save()
-    mlp.plot(y_test, y_pred)
+        logging.info(f"Final test accuracy {mlp.compute_acc(y_pred, y_test)}")
+        mlp.save()
+        mlp.plot(y_test, y_pred)
+
+        print("############################################################################")
